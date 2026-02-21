9974425f88f8c0d03152be7a2757c51a66401f43
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-21 16:25:26 +0600

    Interactive Form design

--- script/login.js
+++ script/login.js
@@ -1,1 +1,34 @@
-console.log('login functionality')
+
+// this output line only checking the line write or wrong
+console.log('login functionality');
+
+//button functionality started
+document.getElementById('login-btn')
+.addEventListener('click',function(){
+
+//input number
+const inputNumber=document.getElementById('input-number');
+const contactNumber=inputNumber.value;
+console.log(contactNumber);
+
+//input pin
+const inputPin=document.getElementById('input-pin');
+const pin=inputPin.value;
+console.log(pin);
+
+// Match phone number or pin
+if(contactNumber=="01916948899" && pin=="1234"){
+    alert('Login successfully');
+    window.location.assign('/home.html');
+}
+else{
+    alert('Login failed');
+    return;
+}
+
+
+
+
+
+})
+
